754de2e4184cf5b851007afb28e022f3100742cc
Administrator <Administrator@10.0.2.15>
2017-03-16 05:08:28 -0700

    Domain model

---
+++ Pintacme/src/main/java/domain/Comment.java
@@ -0,0 +1,35 @@
+
+package domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import security.UserAccount;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public abstract class Comment extends DomainEntity {
+
+	private UserAccount userAccount;
+
+
+	public Comment() {
+		super();
+	}
+
+	@NotNull
+	@Valid
+	@OneToOne(cascade = CascadeType.ALL, optional = false)
+	public UserAccount getUserAccount() {
+		return this.userAccount;
+	}
+
+	public void setUserAccount(final UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
+}
