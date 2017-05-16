677b42b18fa1db9ea18ee88a353fc7c246fd95a2
jesnavino <jesnavino@alum.us.es>
2017-05-16 10:20:57 -0700

    Discussion

---
+++ Pintacme/src/main/java/services/DiscussionService.java
@@ -0,0 +1,34 @@
+package services;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import domain.Discussion;
+import repositories.DiscussionRepository;
+
+@Service
+@Transactional
+public class DiscussionService {
+
+	@Autowired
+	private DiscussionRepository discussionRepository;
+	
+
+	public DiscussionService() {
+		super();
+	}
+	
+
+	public Collection<Discussion> findAll(){
+		Collection<Discussion> res;
+		
+		res = discussionRepository.findAll();
+		
+		return res;
+	}
+
+}
+
